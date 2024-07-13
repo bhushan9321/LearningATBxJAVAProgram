8d89031b8c0cce14c2805716b7d6b47b66408810
Bhushan <bhushan9321@gmail.com>
2024-07-13 19:42:25 +0530

    All File Added

---
+++ src/Ex_07062024/Lab001.java
@@ -0,0 +1,9 @@
+package Ex_07062024;
+
+import java.sql.SQLOutput;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello Word ");
+    }
+}
---
+++ src/Ex_07062024/Lab003.java
@@ -0,0 +1,14 @@
+package Ex_07062024;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+// single comment -this code will not run
+        // 2+2 ?
+        /* multiple comment
+        to
+        high */
+
+
+    }
+}
